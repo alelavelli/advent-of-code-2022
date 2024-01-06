65031a71c37c603637073c127e1e74bfb1526f6b
Alessandro <aleslavelli@gmail.com>
2024-01-06 11:07:49 +0100

    day 3

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::File, io::Read, time::Instant};
+use std::{collections::HashSet, error::Error, fs::File, io::Read, time::Instant};
 
 use log::info;
 
@@ -31,12 +31,47 @@ pub fn solve(puzzle_input: &str, part: ProblemPart) -> Result<(), Box<dyn Error>
     Ok(())
 }
 
-fn solve_pt1(_puzzle_input: String) -> Result<String, Box<dyn Error>> {
-    todo!()
+const LOWER_OFFSET: u8 = 'a' as u8;
+const HIGHER_OFFSET: u8 = 'A' as u8;
+
+fn solve_pt1(puzzle_input: String) -> Result<String, Box<dyn Error>> {
+    let mut priority_sum: i32 = 0;
+    for line in puzzle_input.lines() {
+        let first_compartment = line.chars().take(line.len() / 2).collect::<HashSet<char>>();
+        let second_compartment = line.chars().skip(line.len() / 2).collect::<HashSet<char>>();
+        let item = first_compartment
+            .intersection(&second_compartment)
+            .collect::<Vec<&char>>()[0];
+        let offset = if item.is_ascii_lowercase() {
+            LOWER_OFFSET - 1
+        } else {
+            HIGHER_OFFSET - 27
+        };
+        priority_sum += (*item as u8 - offset) as i32;
+    }
+    Ok(priority_sum.to_string())
 }
 
-fn solve_pt2(_puzzle_input: String) -> Result<String, Box<dyn Error>> {
-    todo!()
+fn solve_pt2(puzzle_input: String) -> Result<String, Box<dyn Error>> {
+    let mut priority_sum: i32 = 0;
+    for group in puzzle_input.lines().collect::<Vec<&str>>().chunks(3) {
+        let badge = group
+            .iter()
+            .map(|&x| x.chars().collect::<HashSet<char>>())
+            .reduce(|a, b| a.intersection(&b).map(|x| *x).collect())
+            .unwrap()
+            .into_iter()
+            .next()
+            .unwrap();
+
+        let offset = if badge.is_ascii_lowercase() {
+            LOWER_OFFSET - 1
+        } else {
+            HIGHER_OFFSET - 27
+        };
+        priority_sum += (badge as u8 - offset) as i32;
+    }
+    Ok(priority_sum.to_string())
 }
 
 #[cfg(test)]
@@ -47,24 +82,24 @@ mod test {
 
     #[test]
     fn test_pt1() -> Result<(), Box<dyn Error>> {
-        let mut file = File::open("inputs/")?;
+        let mut file = File::open("inputs/day_03_example.txt")?;
         let mut puzzle_input = String::new();
         file.read_to_string(&mut puzzle_input)?;
-        let _result = solve_pt1(puzzle_input)?;
+        let result = solve_pt1(puzzle_input)?;
 
-        // Add your assertions
+        assert_eq!(String::from("157"), result);
 
         Ok(())
     }
 
     #[test]
     fn test_pt2() -> Result<(), Box<dyn Error>> {
-        let mut file = File::open("inputs/")?;
+        let mut file = File::open("inputs/day_03_example.txt")?;
         let mut puzzle_input = String::new();
         file.read_to_string(&mut puzzle_input)?;
-        let _result = solve_pt2(puzzle_input)?;
+        let result = solve_pt2(puzzle_input)?;
 
-        // Add your assertions
+        assert_eq!(String::from("70"), result);
 
         Ok(())
     }
