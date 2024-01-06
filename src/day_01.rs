924b218b0ee9e3d4de9857a568b44addbc87fa45
Alessandro <aleslavelli@gmail.com>
2024-01-06 09:05:23 +0100

    day 01

--- src/day_01.rs
+++ src/day_01.rs
@@ -31,12 +31,35 @@ pub fn solve(puzzle_input: &str, part: ProblemPart) -> Result<(), Box<dyn Error>
     Ok(())
 }
 
-fn solve_pt1(_puzzle_input: String) -> Result<String, Box<dyn Error>> {
-    todo!()
+fn solve_pt1(puzzle_input: String) -> Result<String, Box<dyn Error>> {
+    let mut max_calories = 0;
+
+    let mut current_calories = 0;
+    for line in puzzle_input.lines() {
+        if line.len() == 0 {
+            max_calories = max_calories.max(current_calories);
+            current_calories = 0;
+        } else {
+            current_calories += line.parse::<i32>().unwrap();
+        }
+    }
+    Ok(max_calories.to_string())
 }
 
-fn solve_pt2(_puzzle_input: String) -> Result<String, Box<dyn Error>> {
-    todo!()
+fn solve_pt2(puzzle_input: String) -> Result<String, Box<dyn Error>> {
+    let mut calories: Vec<i32> = Vec::new();
+
+    let mut current_calories = 0;
+    for block in puzzle_input.split("\n\n") {
+        for line in block.lines() {
+            current_calories += line.parse::<i32>().unwrap();
+        }
+        calories.push(current_calories);
+        current_calories = 0;
+    }
+    calories.sort();
+    calories.reverse();
+    Ok(calories.iter().take(3).sum::<i32>().to_string())
 }
 
 #[cfg(test)]
@@ -47,24 +70,24 @@ mod test {
 
     #[test]
     fn test_pt1() -> Result<(), Box<dyn Error>> {
-        let mut file = File::open("inputs/")?;
+        let mut file = File::open("inputs/day_01_example.txt")?;
         let mut puzzle_input = String::new();
         file.read_to_string(&mut puzzle_input)?;
-        let _result = solve_pt1(puzzle_input)?;
+        let result = solve_pt1(puzzle_input)?;
 
-        // Add your assertions
+        assert_eq!(String::from("24000"), result);
 
         Ok(())
     }
 
     #[test]
     fn test_pt2() -> Result<(), Box<dyn Error>> {
-        let mut file = File::open("inputs/")?;
+        let mut file = File::open("inputs/day_01_example.txt")?;
         let mut puzzle_input = String::new();
         file.read_to_string(&mut puzzle_input)?;
-        let _result = solve_pt2(puzzle_input)?;
+        let result = solve_pt2(puzzle_input)?;
 
-        // Add your assertions
+        assert_eq!(String::from("45000"), result);
 
         Ok(())
     }
